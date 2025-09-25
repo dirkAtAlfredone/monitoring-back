f2f84714674b46f4eb35421d4e889c891721a90d
Dirk Brandon Lapitan <dirk@alfredone.ca>
2025-09-25 06:51:09 -0400

    initialize a server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello WOrld!!! and again")
+import * as express from "express";
+
+const PORT = 4000;
+const app = express();
+
+app.use("/", (req, res) => res.json("You have reached Backend Server of AlfredOne Monitoring..."));
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}...`))
